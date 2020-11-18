b54bd9d3d6c827671fa0d180d56ddab3dea67d8d
Guy-Moshkowich <guy.moshkowich@gmail.com>
2020-11-18 06:46:31 +0200

    add rotateing debug log

--- kill_ideal_process.py
+++ kill_ideal_process.py
@@ -3,6 +3,7 @@ import os
 import time
 import sys
 import logging
+from logging.handlers import RotatingFileHandler
 
 
 def run_ps_commands(command):
@@ -20,9 +21,26 @@ def run_ps_commands(command):
     return [], []
 
 
+log_level = logging.INFO
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+handler = RotatingFileHandler('killer_ideal_process.log', maxBytes=100000, backupCount=5)
+handler.setFormatter(formatter)
+logger = logging.getLogger("Rotating Log")
+logger.addHandler(handler)
+
 if len(sys.argv) > 1:
     ps_command = sys.argv[1]
     timeout = int(sys.argv[2])
+    log_level_str = sys.argv[3]
+    if log_level_str == 'debug':
+        log_level = logging.DEBUG
+        logger.setLevel(log_level)
+
+# logging.basicConfig(filename='myapp.log', level=log_level)
+logger.info('Started')
+logger.info('ps_command:' + ps_command)
+logger.info('timeout: ' + str(timeout))
 
 
 while True:
@@ -30,19 +48,21 @@ while True:
     if not process_args:
         time.sleep(timeout)
         continue
-    logging.debug(process_args)
+        logger.debug(process_args)
     cpu_time_arg_index = header.index('TIME')
     cpu_start = process_args[cpu_time_arg_index]
-    logging.debug('cpu_start: ' + cpu_start)
+    logger.debug('cpu_start: ' + cpu_start)
     time.sleep(timeout)
     header, process_args = run_ps_commands(ps_command)
-    logging.debug(process_args)
+    logger.debug(process_args)
     if not process_args:
         continue
     cpu_end = process_args[cpu_time_arg_index]
-    logging.debug('cpu_end: ' + cpu_end)
+    logger.debug('cpu_end: ' + cpu_end)
     if cpu_end == cpu_start:
         pid_arg_index = header.index('PID')
         pid = process_args[pid_arg_index]
-        logging.debug(pid)
-        os.system('kill ' + pid)
+        logger.debug('try to kill process: ' + pid)
+        os.system('kill -9 ' + pid)
+    else:
+        logger.debug('there is delta.')
--- kill_ideal_process.sh
+++ kill_ideal_process.sh
@@ -1,2 +1,2 @@
 date > /Users/guymoshkowich/Downloads/kill_ideal_process/app.log
-/Users/guymoshkowich/opt/miniconda3/bin/python /Users/guymoshkowich/Downloads/kill_ideal_process/kill_ideal_process.py Pages 300
+/Users/guymoshkowich/opt/miniconda3/bin/python /Users/guymoshkowich/Downloads/kill_ideal_process/kill_ideal_process.py Pages 300 debug
